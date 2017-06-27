c84e317c8631ea7648a96fc81b42decabcc6264b
icesukhatouch <sukhatouchdenkirati@Sukhatouchs-MacBook-Pro.local>
2017-06-27 16:28:58 +0700

    edit main content

---
+++ sc.js
@@ -0,0 +1,15 @@
+function main(){
+    "use strict";
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+}(jQuery); // End of use strict
+
+$(document).ready(main);
